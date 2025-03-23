10ef28fbac15c5bf7e5c6c209a9b2e2800272db8
Monicarngdar <monica.arong.darmanin@gmail.com>
2025-03-23 11:35:52 +0100

    Added New JS
    Added a new js function for landscape gallery

--- script/modalimage.js
+++ script/modalimage.js
@@ -6,13 +6,26 @@ function showModal(img){
     var modal = document.getElementById("myModal");
     document.getElementById("modal_image").src=img.src; //replacing the image source of the modal img
     document.getElementById("modal_image").alt=img.alt; // placing the alt tag for the modal tag
-    document.getElementById("modal_h6").innerHTML=img.alt;
+   // document.getElementById("modal_h6").innerHTML=img.alt;
     currentIndex = parseInt(img.id.slice(5));
     modal.style.display = "block";
   
   document.body.classList.add('modal-show');
 
   }
+
+  function showModallandscape(img){
+    if (window.innerWidth <= 640 ) return //disabled modal on small screens
+      var modal = document.getElementById("myModal");
+      document.getElementById("modal_image").src=img.src; //replacing the image source of the modal img
+      document.getElementById("modal_image").alt=img.alt; // placing the alt tag for the modal tag
+      document.getElementById("modal_h6").innerHTML=img.alt;
+      currentIndex = parseInt(img.id.slice(5));
+      modal.style.display = "block";
+    
+    document.body.classList.add('modal-show');
+  
+    }
   function closeModal(){
     var modal = document.getElementById("myModal");
       modal.style.display = "none";
